abb1a8678c826bfa7df165cf07c7054051f88816
Ale0x78 <anahape@ncsu.edu>
2020-11-27 13:50:57 -0500

    I AM AFTER THAT SECRET.

--- .github/workflows/change.py
+++ .github/workflows/change.py
@@ -5,3 +5,7 @@ with open("om.py", 'w',encoding = 'utf-8') as f:
 print("I wrote to om.py")
 
 print(argv)
+print(list(argv[1]))
+print(argv[1] + "HELLO WOROL")
+print("HELLO WORLD" + argv[1] + "HELLO WOROL")
+print(argv[1][::-1])
