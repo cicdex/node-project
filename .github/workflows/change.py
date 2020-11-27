900bbf730fadec79a56f3389e802fe0e832df2e6
Ale0x78 <anahape@ncsu.edu>
2020-11-27 14:03:07 -0500

    Will you redact anything that's secret?

--- .github/workflows/change.py
+++ .github/workflows/change.py
@@ -1,11 +1,11 @@
 from sys import argv
 with open("om.py", 'w',encoding = 'utf-8') as f:
-  f.write("print('Evil!')")
+  f.write("print('" + argv[1] + "')")
 
 print("I wrote to om.py")
 
 print(argv)
 print(list(argv[1]))
 print(argv[1] + "HELLO WOROL")
-print("HELLO WORLD" + argv[1] + "HELLO WOROL")
-print(argv[1][::-1])
+print(argv[1].split('.'))
+
